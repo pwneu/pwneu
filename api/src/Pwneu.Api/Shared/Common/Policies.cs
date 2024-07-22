e8948ceb603fa1d985bb4d8959b8a4e93a7fb8b2
David King Roderos <dk.roderos@proton.me>
2024-07-23 00:36:02 +0800

    Remove unused policy

--- api/src/Pwneu.Api/Shared/Common/Policies.cs
+++ api/src/Pwneu.Api/Shared/Common/Policies.cs
@@ -2,7 +2,6 @@ namespace Pwneu.Api.Shared.Common;
 
 public static class Policies
 {
-    public const string Any = "Any";
     public const string UserOnly = "UserOnly";
     public const string FacultyAdminOnly = "FacultyAdminOnly";
     public const string AdminOnly = "AdminOnly";
