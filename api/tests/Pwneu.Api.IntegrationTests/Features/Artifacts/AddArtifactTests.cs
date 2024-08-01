70985837a4fb7f4ce5f883ca9577bc3d3ca5f4da
David King Roderos <dk.roderos@proton.me>
2024-08-02 01:04:24 +0800

    Add artifact tests

---
+++ api/tests/Pwneu.Api.IntegrationTests/Features/Artifacts/AddArtifactTests.cs
@@ -0,0 +1,66 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Pwneu.Api.Features.Artifacts;
+using Pwneu.Api.Shared.Common;
+using Pwneu.Api.Shared.Entities;
+
+namespace Pwneu.Api.IntegrationTests.Features.Artifacts;
+
+[Collection(nameof(IntegrationTestCollection))]
+public class AddArtifactTests(IntegrationTestsWebAppFactory factory) : BaseIntegrationTest(factory)
+{
+    [Fact]
+    public async Task Handle_Should_AddArtifact_WhenChallengeExists()
+    {
+        // Arrange
+        var categoryId = Guid.NewGuid();
+        var category = new Category
+        {
+            Id = categoryId,
+            Name = F.Lorem.Word(),
+            Description = F.Lorem.Sentence()
+        };
+        DbContext.Add(category);
+        await DbContext.SaveChangesAsync();
+
+        var challengeId = Guid.NewGuid();
+        DbContext.Add(new Challenge
+        {
+            Id = challengeId,
+            CategoryId = categoryId,
+            Name = F.Lorem.Word(),
+            Description = F.Lorem.Sentence(),
+            Points = F.Random.Int(1, 100),
+            DeadlineEnabled = F.Random.Bool(),
+            Deadline = DateTime.UtcNow,
+            MaxAttempts = F.Random.Int(1, 10),
+            Flags = F.Lorem.Words().ToList()
+        });
+        await DbContext.SaveChangesAsync();
+
+        var fileContent = "PWNEU{@1Ways_T3$t_yoUr_$OFt@War3}"u8.ToArray();
+        var formStream = new MemoryStream(fileContent);
+        var file = new FormFile(formStream, 0, fileContent.Length, F.Lorem.Word(), F.System.FileName())
+        {
+            Headers = new HeaderDictionary(),
+            ContentType = F.System.MimeType()
+        };
+
+        // Act
+        using var stream = new MemoryStream();
+        await file.CopyToAsync(stream);
+
+        var addArtifact = await Sender.Send(new AddArtifact.Command(
+            ChallengeId: challengeId,
+            FileName: file.FileName,
+            ContentType: file.ContentType,
+            Data: stream.ToArray()));
+
+        var artifact = DbContext.Artifacts.FirstOrDefault(a => a.Id == addArtifact.Value);
+
+        // Assert
+        addArtifact.Should().BeOfType<Result<Guid>>();
+        addArtifact.IsSuccess.Should().BeTrue();
+        artifact.Should().NotBeNull();
+    }
+}
---
+++ api/tests/Pwneu.Api.IntegrationTests/Features/Artifacts/GetArtifactTests.cs
@@ -0,0 +1,78 @@
+using System.Text;
+using FluentAssertions;
+using Pwneu.Api.Features.Artifacts;
+using Pwneu.Api.Shared.Common;
+using Pwneu.Api.Shared.Contracts;
+using Pwneu.Api.Shared.Entities;
+
+namespace Pwneu.Api.IntegrationTests.Features.Artifacts;
+
+[Collection(nameof(IntegrationTestCollection))]
+public class GetArtifactTests(IntegrationTestsWebAppFactory factory) : BaseIntegrationTest(factory)
+{
+    [Fact]
+    public async Task Handle_Should_GetArtifact_WhenArtifactExists()
+    {
+        // Arrange
+        var categoryId = Guid.NewGuid();
+        var category = new Category
+        {
+            Id = categoryId,
+            Name = F.Lorem.Word(),
+            Description = F.Lorem.Sentence()
+        };
+        DbContext.Add(category);
+        await DbContext.SaveChangesAsync();
+
+        var challenge = new Challenge
+        {
+            Id = Guid.NewGuid(),
+            CategoryId = categoryId,
+            Name = F.Lorem.Word(),
+            Description = F.Lorem.Sentence(),
+            Points = F.Random.Int(1, 100),
+            DeadlineEnabled = F.Random.Bool(),
+            Deadline = DateTime.UtcNow,
+            MaxAttempts = F.Random.Int(1, 10),
+            Flags = F.Lorem.Words().ToList()
+        };
+        DbContext.Add(challenge);
+
+        var artifact = new Artifact
+        {
+            Id = Guid.NewGuid(),
+            ChallengeId = challenge.Id,
+            FileName = F.System.FileName(),
+            ContentType = F.System.MimeType(),
+            Data = Encoding.UTF8.GetBytes(F.Lorem.Text()),
+            Challenge = challenge
+        };
+        DbContext.Add(artifact);
+
+        await DbContext.SaveChangesAsync();
+
+        var artifactData = new ArtifactDataResponse(
+            FileName: artifact.FileName,
+            ContentType: artifact.ContentType,
+            Data: artifact.Data);
+
+        // Act
+        var getArtifact = await Sender.Send(new GetArtifact.Query(artifact.Id));
+
+        // Assert
+        getArtifact.Should().NotBeNull();
+        getArtifact.Should().BeOfType<Result<ArtifactDataResponse>>();
+        getArtifact.Value.Should().BeEquivalentTo(artifactData);
+    }
+
+    [Fact]
+    public async Task Handle_Should_NotGetArtifact_WhenArtifactDoesNotExists()
+    {
+        // Act
+        var getArtifact = new GetArtifact.Query(Guid.NewGuid());
+        var artifact = await Sender.Send(getArtifact);
+
+        // Assert
+        artifact.IsSuccess.Should().BeFalse();
+    }
+}
