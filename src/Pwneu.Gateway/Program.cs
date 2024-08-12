b3b1db5974f2c8ae006a81d44e5951687d4a7923
David King Roderos <dk.roderos@proton.me>
2024-08-12 15:03:08 +0800

    Add api gateway

---
+++ src/Pwneu.Gateway/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+app.Run();
