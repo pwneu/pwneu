35db18769229a7da4b4cce76cdd55c70029ff44d
David King Roderos <dk.roderos@proton.me>
2024-08-28 23:52:01 +0800

    Update user details tests

--- tests/Pwneu.Identity.IntegrationTests/Features/Users/GetUserDetailsTests.cs
+++ tests/Pwneu.Identity.IntegrationTests/Features/Users/GetUserDetailsTests.cs
@@ -5,23 +5,22 @@ using Pwneu.Shared.Contracts;
 
 namespace Pwneu.Identity.IntegrationTests.Features.Users;
 
+// TODO -- Add more tests
+
 [Collection(nameof(IntegrationTestCollection))]
 public class GetUserDetailsTests(IntegrationTestsWebAppFactory factory) : BaseIntegrationTest(factory)
 {
-    [Theory]
-    [InlineData(Consts.Admin, false)]
-    [InlineData("test", true)]
-    public async Task Handle_Should_GetUser_WhenUserIsNotManagerOrAdmin(string userName, bool expected)
+    [Fact]
+    public async Task Handle_Should_GetUser_WhenUserExists()
     {
         // Arrange
         await Cache.RemoveAsync("managerIds");
-        var user = await UserManager.FindByNameAsync(userName);
+        var user = await UserManager.FindByNameAsync("test");
 
         // Act
         var getUser = await Sender.Send(new GetUserDetails.Query(user!.Id));
 
         // Assert
-        getUser.IsSuccess.Should().Be(expected);
         getUser.Should().BeOfType<Result<UserDetailsResponse>>();
     }
 }
