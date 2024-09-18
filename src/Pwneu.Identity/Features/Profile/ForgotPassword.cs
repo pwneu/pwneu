defabb1b81045b7d8f76bd383aaa920e40acd597
David King Roderos <dk.roderos@proton.me>
2024-09-18 10:19:57 +0800

    Disable password reset on unverified emails

--- src/Pwneu.Identity/Features/Profile/ForgotPassword.cs
+++ src/Pwneu.Identity/Features/Profile/ForgotPassword.cs
@@ -2,23 +2,39 @@ using FluentValidation;
 using MassTransit;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 using Pwneu.Identity.Shared.Entities;
+using Pwneu.Identity.Shared.Options;
 using Pwneu.Shared.Common;
 using Pwneu.Shared.Contracts;
 
 namespace Pwneu.Identity.Features.Profile;
 
+/// <summary>
+/// Generates a password reset and sent via email.
+/// If email verification is disabled, this will also be disabled.
+/// </summary>
 public static class ForgotPassword
 {
     public record Command(string Email) : IRequest<Result>;
 
+    private static readonly Error EmailVerificationDisabled =
+        new("ForgotPassword.EmailVerificationDisabled",
+            "Unfortunately, email verification is disabled, forgot password via email is not allowed.");
+
     internal sealed class Handler(
         UserManager<User> userManager,
         IPublishEndpoint publishEndpoint,
-        IValidator<Command> validator) : IRequestHandler<Command, Result>
+        IValidator<Command> validator,
+        IOptions<AppOptions> appOptions) : IRequestHandler<Command, Result>
     {
+        private readonly AppOptions _appOptions = appOptions.Value;
+
         public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
         {
+            if (!_appOptions.RequireEmailVerification)
+                return Result.Failure(EmailVerificationDisabled);
+
             var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
             if (!validationResult.IsValid)
@@ -28,6 +44,9 @@ public static class ForgotPassword
 
             // Don't give the requester a clue if the user exists with the specified email.
             if (user is null)
+                return Result.Success();
+
+            if (!user.EmailConfirmed)
                 return Result.Success();
 
             var token = await userManager.GeneratePasswordResetTokenAsync(user);
