9bbbee11a229330cd3d68de510da085ab4f0eb01
David King Roderos <dk.roderos@proton.me>
2024-10-14 00:54:59 +0800

    Implement getting user rank

---
+++ src/Pwneu.Play/Features/Users/GetUserRank.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Pwneu.Play.Shared.Data;
+using Pwneu.Play.Shared.Extensions;
+using Pwneu.Play.Shared.Services;
+using Pwneu.Shared.Common;
+using Pwneu.Shared.Contracts;
+using Pwneu.Shared.Extensions;
+using ZiggyCreatures.Caching.Fusion;
+
+namespace Pwneu.Play.Features.Users;
+
+public class GetUserRank
+{
+    public record Query(string Id) : IRequest<Result<UserRankResponse?>>;
+
+    private static readonly Error NotFound = new(
+        "GetUserRank.NotFound",
+        "The user with the specified ID was not found");
+
+    internal sealed class Handler(ApplicationDbContext context, IFusionCache cache, IMemberAccess memberAccess)
+        : IRequestHandler<Query, Result<UserRankResponse?>>
+    {
+        public async Task<Result<UserRankResponse?>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            // Check if user exists.
+            if (!await memberAccess.MemberExistsAsync(request.Id, cancellationToken))
+                return Result.Failure<UserRankResponse?>(NotFound);
+
+            var userRanks = await cache.GetOrSetAsync(
+                Keys.UserRanks(),
+                async _ => await context.GetUserRanks(cancellationToken),
+                new FusionCacheEntryOptions { Duration = TimeSpan.FromMinutes(20) },
+                cancellationToken);
+
+            var requesterRank = userRanks.FirstOrDefault(u => u.Id == request.Id);
+
+            return requesterRank;
+        }
+    }
+
+    public class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("users/{id:Guid}/rank", async (Guid id, ISender sender) =>
+                {
+                    var query = new Query(id.ToString());
+                    var result = await sender.Send(query);
+
+                    return result.IsFailure ? Results.NotFound(result.Error) : Results.Ok(result.Value);
+                })
+                .RequireAuthorization(Consts.ManagerAdminOnly)
+                .WithTags(nameof(Users));
+
+            app.MapGet("me/rank", async (ClaimsPrincipal claims, ISender sender) =>
+                {
+                    var id = claims.GetLoggedInUserId<string>();
+                    if (id is null) return Results.BadRequest();
+
+                    var query = new Query(id);
+                    var result = await sender.Send(query);
+
+                    return result.IsFailure ? Results.NotFound(result.Error) : Results.Ok(result.Value);
+                })
+                .RequireAuthorization()
+                .WithTags(nameof(Users));
+        }
+    }
+}
