a5aa1a3bf0168780d67b5b9ff6f7d1d562654d73
David King Roderos <dk.roderos@proton.me>
2024-11-15 23:28:13 +0800

    Fix error on user cleanup service

--- src/Pwneu.Identity/Workers/UserCleanupService.cs
+++ src/Pwneu.Identity/Workers/UserCleanupService.cs
@@ -12,10 +12,7 @@ namespace Pwneu.Identity.Workers;
 /// </summary>
 /// <param name="serviceProvider">Service provider.</param>
 /// <param name="logger">Logger.</param>
-public class UserCleanupService(
-    IServiceProvider serviceProvider,
-    ILogger<UserCleanupService> logger,
-    ISender sender)
+public class UserCleanupService(IServiceProvider serviceProvider, ILogger<UserCleanupService> logger)
     : BackgroundService
 {
     private readonly TimeSpan _cleanupInterval = TimeSpan.FromHours(24);
@@ -31,27 +28,35 @@ public class UserCleanupService(
 
     private async Task CleanupUnconfirmedAccountsAsync(CancellationToken cancellationToken)
     {
-        logger.LogInformation("Started deleting unconfirmed users");
-        using var scope = serviceProvider.CreateScope();
-        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
-        var users = await userManager.Users
-            .Where(u => !u.EmailConfirmed && u.CreatedAt < DateTime.UtcNow.AddHours(-24))
-            .ToListAsync(cancellationToken);
-
-        // If admin is null we're screwed (someone tampered with the database directly).
-        // But it should've been created at this point.
-        var admin = await userManager.FindByNameAsync(Consts.Admin);
-
-        foreach (var user in users)
+        try
         {
-            var deleteUser = new DeleteUser.Command(user.Id, admin?.Id ?? Guid.NewGuid().ToString());
+            logger.LogInformation("Started deleting unconfirmed users");
+            using var scope = serviceProvider.CreateScope();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+            var sender = scope.ServiceProvider.GetRequiredService<ISender>();
+            var users = await userManager.Users
+                .Where(u => !u.EmailConfirmed && u.CreatedAt < DateTime.UtcNow.AddHours(-24))
+                .ToListAsync(cancellationToken);
 
-            var result = await sender.Send(deleteUser, cancellationToken);
+            // If admin is null we're screwed (someone tampered with the database directly).
+            // But it should've been created at this point.
+            var admin = await userManager.FindByNameAsync(Consts.Admin);
 
-            if (result.IsSuccess)
-                logger.LogInformation("Deleted unconfirmed user: {email}", user.Email);
-            else
-                logger.LogError("Failed to delete unconfirmed user: {email}", user.Email);
+            foreach (var user in users)
+            {
+                var deleteUser = new DeleteUser.Command(user.Id, admin?.Id ?? Guid.NewGuid().ToString());
+
+                var result = await sender.Send(deleteUser, cancellationToken);
+
+                if (result.IsSuccess)
+                    logger.LogInformation("Deleted unconfirmed user: {email}", user.Email);
+                else
+                    logger.LogError("Failed to delete unconfirmed user: {email}", user.Email);
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogInformation("Something went wrong deleting unconfirmed users: {message}", ex.Message);
         }
     }
 }
