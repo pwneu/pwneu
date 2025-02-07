c99c2e343e36ca02f2ab2c570d3ec563dc4b73e7
David King Roderos <dk.roderos@proton.me>
2025-02-07 21:44:06 +0800

    Update username validation on login form

--- src/Pwneu.Identity/Features/Auths/Login.cs
+++ src/Pwneu.Identity/Features/Auths/Login.cs
@@ -240,7 +240,13 @@ public static class Login
         {
             RuleFor(c => c.UserName)
                 .NotEmpty()
-                .WithMessage("Username is required.");
+                .WithMessage("Username is required.")
+                .MinimumLength(5)
+                .WithMessage("Username must be at least 5 characters long.")
+                .MaximumLength(40)
+                .WithMessage("Username must be 40 characters or less.")
+                .Matches(@"^[a-zA-Z0-9]+$")
+                .WithMessage("Username can only contain letters and numbers.");
 
             RuleFor(c => c.Password)
                 .NotEmpty()
