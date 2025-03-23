a82ec7690d4cf7b1576e6da355fd4dbfadb2b835
David King Roderos <dk.roderos@gmail.com>
2025-03-23 16:01:39 +0800

    Disable health check on production

--- src/Pwneu.ServiceDefaults/Extensions.cs
+++ src/Pwneu.ServiceDefaults/Extensions.cs
@@ -114,18 +114,18 @@ public static class Extensions
     {
         // Adding health checks endpoints to applications in non-development environments has security implications.
         // See https://aka.ms/dotnet/aspire/healthchecks for details before enabling these endpoints in non-development environments.
-        //if (app.Environment.IsDevelopment())
-        //{
-        // All health checks must pass for app to be considered ready to accept traffic after starting
-        app.MapHealthChecks("/health");
-        app.UseOpenTelemetryPrometheusScrapingEndpoint();
+        if (app.Environment.IsDevelopment())
+        {
+            // All health checks must pass for app to be considered ready to accept traffic after starting
+            app.MapHealthChecks("/health");
+            app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
-        // Only health checks tagged with the "live" tag must pass for app to be considered alive
-        app.MapHealthChecks(
-            "/alive",
-            new HealthCheckOptions { Predicate = r => r.Tags.Contains("live") }
-        );
-        //}
+            // Only health checks tagged with the "live" tag must pass for app to be considered alive
+            app.MapHealthChecks(
+                "/alive",
+                new HealthCheckOptions { Predicate = r => r.Tags.Contains("live") }
+            );
+        }
 
         return app;
     }
