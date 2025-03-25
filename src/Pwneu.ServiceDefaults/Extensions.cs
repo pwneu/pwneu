50503186311aaf61ed35941ab243adf3bcef2395
David King Roderos <dk.roderos@gmail.com>
2025-03-25 20:54:41 +0800

    Reenable telemetry on production

--- src/Pwneu.ServiceDefaults/Extensions.cs
+++ src/Pwneu.ServiceDefaults/Extensions.cs
@@ -112,13 +112,14 @@ public static class Extensions
 
     public static WebApplication MapDefaultEndpoints(this WebApplication app)
     {
+        app.UseOpenTelemetryPrometheusScrapingEndpoint();
+
         // Adding health checks endpoints to applications in non-development environments has security implications.
         // See https://aka.ms/dotnet/aspire/healthchecks for details before enabling these endpoints in non-development environments.
         if (app.Environment.IsDevelopment())
         {
             // All health checks must pass for app to be considered ready to accept traffic after starting
             app.MapHealthChecks("/health");
-            app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
             // Only health checks tagged with the "live" tag must pass for app to be considered alive
             app.MapHealthChecks(
